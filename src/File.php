b3470106f1c4a17ceac068d20faeb1fdaf02f045
Mikhail Ilinsky <mail@milinsky.ru>
2025-05-17 19:57:24 +1000

    Rename File methods

--- src/File.php
+++ src/File.php
@@ -13,14 +13,14 @@ use Fiber;
  */
 final class File
 {
-    public static function get(string $path): Future
+    public static function read(string $path): Future
     {
         $task = new ReadFileTask();
         $task->setPath($path);
         return Fiber::suspend($task);
     }
 
-    public static function put(string $path, string $contents): Future
+    public static function write(string $path, string $contents): Future
     {
         $task = new WriteFileTask();
         $task->setPath($path);
